527cd4fce3730ddf5404b9776676273deb769c85
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-09-30 12:03:32 +0700

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Login from '@/components/ui/Login'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <Login />
+    )
+}
+
+export default page
