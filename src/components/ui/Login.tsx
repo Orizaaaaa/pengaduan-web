75d38c95b7ff1e5019b6336ae0ddafc3256e6f42
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-09-30 12:02:10 +0700

    add login pages

--- src/components/ui/Login.tsx
+++ src/components/ui/Login.tsx
@@ -1,11 +1,91 @@
-import React from 'react'
+"use client"; // Pastikan seluruh komponen ini dirender di sisi klien
 
-type Props = {}
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import InputForm from '@/components/elements/input/InputForm';
+import ButtonPrimary from '@/components/elements/buttonPrimary';
+import { FaEyeSlash } from 'react-icons/fa6';
+import { IoEye } from 'react-icons/io5';
+import { loginService } from '@/api/auth';
+import { Spinner } from '@nextui-org/react';
+import { logo } from '@/app/image';
 
-const Login = (props: Props) => {
+const Login = () => {
+    const router = useRouter();
+    const [showPassword, setShowPassword] = useState(true);
+    const [errorLogin, setErrorLogin] = useState('');
+    const [disabled, setDisabled] = useState(true);
+    const [typePassword, setTypePassword] = useState("password");
+    const [loading, setLoading] = useState(false);
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const togglePassword = () => {
+        setShowPassword(!showPassword);
+        setTypePassword(showPassword ? "text" : "password");
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+
+        const updatedValues = {
+            ...form,
+            [name]: value,
+        };
+
+        // Update the disabled state based on the form values
+        setDisabled(!(updatedValues.email.includes('@gmail.com') || updatedValues.email.includes('@test.com')) || updatedValues.password === "");
+    };
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+
+        await loginService(form, (status: boolean, res: any) => {
+            setLoading(false);
+            if (status) {
+                const tokenCookies = `token=${res.data.token}`;
+                document.cookie = tokenCookies; // Set cookie
+                // Akses localStorage hanya di sisi klien
+                localStorage.setItem('name', res.data.username);
+                localStorage.setItem('role', res.data.role);
+                localStorage.setItem('token', res.data.token)
+                router.push('/dashboard');
+            } else {
+                setErrorLogin('*Email atau password salah');
+                console.log(res.data);
+            }
+        });
+    };
+
+
     return (
-        <div>hllllooooo</div>
-    )
-}
+        <div className="login">
+            <div className="container mx-auto flex justify-center items-center w-[100vw] h-[100vh] ">
+                <form className='p-6 bg-[#e9e9e9] rounded-lg w-96 m-3 lg:m-0' onSubmit={handleLogin}>
+                    <div className="logo flex justify-center my-5">
+                        <Image src={logo} alt="logo" width={70} height={70} />
+                    </div>
 
-export default Login
+                    <InputForm placeholder='Masukkan Email' type='email' htmlFor={'email'} value={form.email} onChange={handleChange} />
+                    <div className="relative">
+                        <button onClick={togglePassword} type='button' className='icon-password h-full  bg-transparent flex absolute right-0 justify-center items-center pe-4'>
+                            {showPassword ? <FaEyeSlash size={20} color='#636363' /> : <IoEye size={20} color='#636363' />}
+                        </button>
+                        <InputForm className='form-input-login' htmlFor="password" onChange={handleChange} type={typePassword} value={form.password} placeholder="Masukkan Kata Sandi" />
+                    </div>
+                    <p className='text-red my-3 text-sm'>{errorLogin}</p>
+                    <ButtonPrimary typeButon={"submit"} disabled={disabled} className={`rounded-lg w-full mb-3 font-medium py-2`}>
+                        {loading ? <Spinner className={`w-5 h-5`} size="sm" color="white" /> : 'Sign In'}
+                    </ButtonPrimary>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
