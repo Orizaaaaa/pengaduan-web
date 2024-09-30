c8f84b476d4b60f1e0e840411ebf5f4212991935
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-09-30 10:08:01 +0700

    create pengaduan

---
+++ src/app/pengaduan/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <section id='pengaduan' className='container mx-auto'>
+            <h1>Pengaduan</h1>
+        </section>
+    )
+}
+
+export default page
