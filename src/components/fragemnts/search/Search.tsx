b6ec58e6829275b2c5fb5a340edecc1788476628
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-02 13:37:07 +0700

    finish articles

---
+++ src/components/fragemnts/search/Search.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React, { useState } from 'react'
+import { IoSearch } from 'react-icons/io5';
+
+type Props = {
+    placeholder: string
+}
+
+const Search = ({ placeholder }: Props) => {
+    const [searchData, setSearchData] = useState("");
+    const handleSearch = (e: any) => {
+        setSearchData(e.target.value);
+    };
+    return (
+        <div className="w-full mt-4 relative ">
+            <input onChange={handleSearch} className="w-full rounded-xl bg-slate-200 outline-none py-2 ps-11" type="text" placeholder={placeholder} name="" id="" />
+            <IoSearch size={20} color="#7C7C7C" className="absolute left-3 top-1/2 -translate-y-1/2" />
+        </div>
+    )
+}
+
+export default Search
--- src/components/ui/Article.tsx
+++ src/components/ui/Article.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { bgPengaduan, human1, image1 } from '@/app/image'
 import Navbar from '../fragemnts/navbar/Navbar'
 import Footer from '../fragemnts/LandingPage/footer/Footer'
+import Search from '../fragemnts/search/Search'
 
 type Props = {}
 
@@ -12,13 +13,14 @@ const Article = (props: Props) => {
         <>
             <Navbar />
             <section className='container mx-auto my-20'>
-                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                <Search placeholder='Cari artikel...' />
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-7">
                     <CardLink href="/">
                         <div className="images h-[150px] ">
                             <Image className='rounded-t-lg w-full h-full' src={bgPengaduan} alt="jalan rusak" />
                         </div>
                         <div className="text px-2 py-1 space-y-2">
-                            <h1 className='font-medium' >Contoh Diskusi Yang Efektif</h1>
+                            <h1 className='font-medium text-sm md:text-base' >Contoh Diskusi Yang Efektif</h1>
                             <p className='text-sm text-slate-400' >Jenis diskusi yang efektif dapat di lakukan secara online</p>
                         </div>
                     </CardLink>
