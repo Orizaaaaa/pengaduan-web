5d1a1e083cd0d3e17d1a862ac6eb3483c7cebfd2
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-03 10:03:36 +0700

    finish page toko online

--- src/components/ui/Shop.tsx
+++ src/components/ui/Shop.tsx
@@ -6,11 +6,13 @@ import { kategoriShop } from '@/utils/dataObject'
 import Search from '../fragemnts/search/Search'
 import CardLink from '../elements/card/CardLink'
 import Image from 'next/image'
-import { bgPengaduan } from '@/app/image'
+import { bgPengaduan, human1 } from '@/app/image'
 import { MdOutlinePersonPin, MdOutlinePersonPinCircle } from 'react-icons/md'
 import { IoPersonCircleSharp, IoPersonSharp } from 'react-icons/io5'
 import ButtonPrimary from '../elements/buttonPrimary'
 import { HiMapPin } from 'react-icons/hi2'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
 
 type Props = {}
 
@@ -36,10 +38,33 @@ const Shop = (props: Props) => {
                 </div>
 
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-7">
-                    <CardLink href="/">
-                        <div className="images h-[150px] ">
-                            <Image className='rounded-t-lg w-full h-full' src={bgPengaduan} alt="jalan rusak" />
-                        </div>
+                    <div className="rounded-md hover: border-stroke bg-white  shadow-default 
+                      dark:border-strokedark ">
+                        <Swiper
+                            slidesPerView={1} // Jumlah default slide yang ditampilkan
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true, // Pagination akan bisa diklik
+                            }}
+                            modules={[Pagination]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide >
+                                <div className="images h-[150px] ">
+                                    <Image className='rounded-t-lg w-full h-full' src={bgPengaduan} alt="jalan rusak" />
+                                </div>
+
+                            </SwiperSlide>
+                            <SwiperSlide >
+                                <div className="images h-[150px] ">
+                                    <Image className='rounded-t-lg w-full h-full' src={human1} alt="jalan rusak" />
+                                </div>
+
+                            </SwiperSlide>
+
+
+                        </Swiper>
+
                         <div className="text px-2 py-1 space-y-1">
                             <h1 className=' text-sm ' >Domba</h1>
                             <h1 className='font-semibold'>Rp 1.000.000</h1>
@@ -56,9 +81,9 @@ const Shop = (props: Props) => {
 
                         </div>
                         <div className="flex px-2 justify-end py-2 items-center">
-                            <ButtonPrimary className='py-1 px-4  rounded-full'>Beli Sekarang</ButtonPrimary>
+                            <ButtonPrimary className='py-2 px-4   rounded-md'>Beli Sekarang</ButtonPrimary>
                         </div>
-                    </CardLink>
+                    </div>
                 </div>
             </section>
         </>
