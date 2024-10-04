0fb706a3609efed8b5658272440150c340fb83f8
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-04 15:18:34 +0700

    finish page laporan and update navigation list

--- src/components/fragemnts/NavigationList/NavigationList.tsx
+++ src/components/fragemnts/NavigationList/NavigationList.tsx
@@ -8,14 +8,24 @@ type Props = {
     icon: any,
 }
 
-const NavigationList = ({ pathname, title, icon, }: Props) => {
+const NavigationList = ({ pathname, title, icon }: Props) => {
     const pathnames = usePathname();
+
+    // Pisahkan URL menjadi array segmen berdasarkan "/"
+    const currentPathSegments = pathnames.split('/').filter(Boolean);
+    const targetPathSegments = pathname.split('/').filter(Boolean);
+
+    // Cek apakah path saat ini cocok dengan path navigasi atau merupakan parent-nya
+    const isActive =
+        currentPathSegments.length === targetPathSegments.length &&
+        currentPathSegments.every((segment, index) => segment === targetPathSegments[index]);
+
     return (
         <li>
             <Link
                 href={`${pathname}`}
                 className={`group relative flex items-center gap-2.5 rounded-lg px-4 py-2 font-medium text-bodydark2 duration-300 text-gray-500 
-                 ease-in-out hover:bg-primary hover:text-white ${pathnames.includes(pathname) &&
+                 ease-in-out hover:bg-primary hover:text-white ${isActive &&
                     "bg-primary text-white"
                     }`}
             >
@@ -23,7 +33,7 @@ const NavigationList = ({ pathname, title, icon, }: Props) => {
                 {title}
             </Link>
         </li>
-    )
+    );
 }
 
-export default NavigationList
+export default NavigationList;
