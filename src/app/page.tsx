55a8053082e064ed232b8f148d35ea25e23fcdc7
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-04 13:11:25 +0700

    fixing bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-// import LandingPage from '@/components/ui/LandingPage'
+import LandingPage from '@/components/ui/LandingPage'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <h1>hallo</h1>
+    <LandingPage />
   )
 }
 
