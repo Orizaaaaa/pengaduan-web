2361aa552515606b1c4860361c7b05acd296cff3
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-10 22:40:35 +0700

    finish protection routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,52 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-//nanti fitur selanjut nya adalah menyeting token expire lewat jwt, jika token expired maka akan di alihkan ke halaman login
+// Middleware untuk mengecek role dan token
 export function middleware(req: NextRequest) {
     const token = req.cookies.get('token');
+    const roleCookie = req.cookies.get('role'); // Ambil role dari cookie
+    const role = roleCookie ? roleCookie.value : undefined; // Ambil value dari cookie jika ada
 
-    // If the request is for the login page, skip the middleware
+    // Jika user mengakses halaman login, abaikan middleware
     if (req.nextUrl.pathname === '/') {
         return NextResponse.next();
     }
 
+    // Jika token tidak ada, redirect ke halaman login
     if (!token) {
-        // Redirect user to login page if not authenticated
         const url = req.nextUrl.clone();
         url.pathname = '/';
         return NextResponse.redirect(url);
     }
 
-    // Continue to the requested page
+    // Logika untuk cek role dan route yang diakses
+    const urlPath = req.nextUrl.pathname;
+
+    // Cek jika role 'officer' mengakses dashboard-officer
+    if (urlPath.startsWith('/dashboard-officer') && role !== 'admin') {
+        const url = req.nextUrl.clone();
+        url.pathname = '/';
+        return NextResponse.redirect(url);
+    }
+
+    // Cek jika role 'superadmin' mengakses dashboard-super-admin
+    if (urlPath.startsWith('/dashboard-super-admin') && role !== 'superadmin') {
+        const url = req.nextUrl.clone();
+        url.pathname = '/';
+        return NextResponse.redirect(url);
+    }
+
+    // Cek jika role 'user' mengakses dashboard-user
+    if (urlPath.startsWith('/dashboard-user') && role !== 'user') {
+        const url = req.nextUrl.clone();
+        url.pathname = '/';
+        return NextResponse.redirect(url);
+    }
+
+    // Lanjut ke halaman yang di-request jika semuanya sesuai
     return NextResponse.next();
 }
 
+// Define paths where the middleware should be applied
 export const config = {
-    matcher: ['/dashboard/:path*', '/bukuBesar/:path*', '/catatTransaksi/:path*',
-        '/jurnalPenutupan/:path*', '/jurnalUmum/:path*', '/labaRugi/:path*', '/listAccount/:path*', '/neracaSaldo/:path*',], // Define paths where the middleware should be applied
+    matcher: ['/dashboard-officer/:path*', '/dashboard-super-admin/:path*', '/dashboard-user/:path*'],
 };
