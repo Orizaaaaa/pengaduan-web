5e628b6fb3249f66043618650c759b7e3149f786
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-15 15:23:16 +0700

    fixing bug and create page pembangunan

--- src/app/dashboard-super-admin/building/page.tsx
+++ src/app/dashboard-super-admin/building/page.tsx
@@ -1,12 +1,74 @@
+'use client'
+import { bgPengaduan, pembangunan } from '@/app/image'
+import ButtonPrimary from '@/components/elements/buttonPrimary'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
+import { HiMapPin } from 'react-icons/hi2'
+import { IoIosArrowForward } from 'react-icons/io'
+import { MdOutlineCalendarToday } from 'react-icons/md'
+import { Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 type Props = {}
 
 const page = (props: Props) => {
+    const router = useRouter()
     return (
         <DefaultLayout>
-            <p>haii</p>
+            <div className="flex justify-end">
+                <ButtonPrimary className='py-2 px-4 rounded-md' >Tambah Pembangunan</ButtonPrimary>
+
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-7">
+                <div className="rounded-md hover: border-stroke bg-white  shadow-default  dark:border-strokedark ">
+                    <Swiper
+                        slidesPerView={1} // Jumlah default slide yang ditampilkan
+                        spaceBetween={30}
+                        pagination={{
+                            clickable: true, // Pagination akan bisa diklik
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
+
+                        <SwiperSlide >
+                            <div className="images h-[150px] ">
+                                <Image className='rounded-t-lg w-full h-full' src={pembangunan} alt="jalan rusak" />
+                            </div>
+
+                        </SwiperSlide>
+
+                        <SwiperSlide >
+                            <div className="images h-[150px] ">
+                                <Image className='rounded-t-lg w-full h-full' src={bgPengaduan} alt="jalan rusak" />
+                            </div>
+
+                        </SwiperSlide>
+
+                    </Swiper>
+
+                    <div className="text px-2 py-1 space-y-1">
+                        <h1 className='font-semibold'>Pembuatan aula desa</h1>
+
+                        <div className="flex  items-center gap-1">
+                            <MdOutlineCalendarToday color='#94a3b8' />
+                            <p className='text-sm text-slate-400' >2-4-2024</p>
+                        </div>
+
+                        <div className="flex  items-center gap-1">
+                            <HiMapPin color='#94a3b8' size={15} />
+                            <p className='text-small text-slate-400' >Kp. Tegalkiang No.1 Jawa Barat...</p>
+                        </div>
+
+                    </div>
+                    <div className="flex px-2 justify-end py-2 items-center gap-3 mt-2">
+                        <ButtonPrimary onClick={() => router.push('/dashboard-super-admin/building/12')} className='py-2 px-4 flex justify-center items-center   rounded-xl text-sm lg:text-base'>Selengkap nya <IoIosArrowForward /> </ButtonPrimary>
+                    </div>
+                </div>
+            </div>
+
         </DefaultLayout>
     )
 }
--- src/app/dashboard-user/shop/page.tsx
+++ src/app/dashboard-user/shop/page.tsx
@@ -1,25 +1,138 @@
 'use client'
 import { url } from '@/api/auth';
 import { fetcher } from '@/api/fetcher';
+import { postImagesArray } from '@/api/imagePost';
+import { createProduct } from '@/api/shop';
 import { bannerShop } from '@/app/image';
 import ButtonPrimary from '@/components/elements/buttonPrimary';
 import CardHover from '@/components/elements/card/CardHover';
 import Search from '@/components/fragemnts/search/Search';
 import DefaultLayout from '@/components/layouts/DefaultLayout'
 import { categoryCaraosel } from '@/utils/dataObject';
+import { useDisclosure } from '@nextui-org/react';
 import Image from 'next/image';
-import React from 'react'
+import React, { useState } from 'react'
 import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
 
 type Props = {}
 
 const Page = (props: Props) => {
+    const [selectedCategory, setSelectedCategory] = useState<string>('Semua Kategori');
+    const [searchData, setSearchData] = useState("");
+    const [loading, setLoading] = useState(false)
+    const idUser: string = localStorage.getItem('id') || '';
+    const { onOpen, onClose, isOpen } = useDisclosure();
     const { data, error } = useSWR(`${url}/shop/list`, fetcher, {
         keepPreviousData: true,
     });
+    const [errorMsg, setErrorMsg] = useState('')
+    const [form, setForm] = useState({
+        name: '',
+        description: '',
+        address: '',
+        price: '',
+        image: [] as File[],
+        category: '',
+        quantity: '',
+        user: idUser
+    })
     const dataShop = data?.data
+
+    const openModalCreate = () => {
+        onOpen()
+    }
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+
+        if (name === 'price' || name === 'quantity') {
+            setForm({
+                ...form,
+                [name]: value === '' ? '' : Number(value) // Konversi ke number jika ada nilai
+            });
+        } else {
+            setForm({
+                ...form,
+                [name]: value // Tetap sebagai string untuk field lainnya
+            });
+        }
+    };
+
+    const dataDropdown = () => {
+        return categoryCaraosel
+            .filter(category => category.title !== 'Semua Kategori') // Menghilangkan 'Semua Kategori'
+            .map((category) => ({
+                label: category.title,
+                value: category.title,
+            }));
+    };
+
+
+    const onSelectionChange = (key: string) => {
+        setForm({ ...form, category: key })  // Menyimpan nilai yang dipilih ke dalam state
+    }
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>, InputSelect: string) => {
+        if (InputSelect === 'add') {
+            const selectedImage = e.target.files?.[0];
+            if (selectedImage) {
+                setForm(prevState => ({
+                    ...prevState,
+                    image: [...prevState.image, selectedImage]
+                }));
+            }
+        }
+    };
+
+    const deleteArrayImage = (index: number, type: string) => {
+        if (type === 'add') {
+            setForm(prevState => ({
+                ...prevState,
+                image: prevState.image.filter((_, i) => i !== index)
+            }));
+        }
+
+
+    };
+
+    const handleCreate = async (e: any) => {
+        e.preventDefault();
+        setLoading(true);
+        const imageUrl: string[] = await postImagesArray({ images: form.image })
+        const data = {
+            ...form,
+            image: imageUrl
+        };
+        if (imageUrl) {
+            createProduct(data, (result: any) => {
+                console.log(result);
+                mutate(`${url}/shop/list`)
+                onClose();
+                setLoading(false);
+            })
+        }
+    }
+
+    const handleSearch = (e: any) => {
+        setSearchData(e.target.value);
+    };
+
+
+    // Fungsi untuk menangani klik kategori
+    const handleCategoryClick = (category: string) => {
+        setSelectedCategory(category);
+    };
+
+    // Filter data berdasarkan kategori yang dipilih
+    const filteredData = dataShop?.filter((item: any) => {
+        const isCategoryMatch = selectedCategory === 'Semua Kategori' || item.category === selectedCategory;
+        const isSearchMatch = item.name.toLowerCase().includes(searchData.toLowerCase()) ||
+            item.description.toLowerCase().includes(searchData.toLowerCase());
+        return isCategoryMatch && isSearchMatch;
+    });
+
     return (
         <DefaultLayout>
 
@@ -43,7 +156,7 @@ const Page = (props: Props) => {
             <section className='my-10' >
                 <h1 className='text-2xl font-bold my-10'>Kategori</h1>
                 <Swiper
-                    slidesPerView={7} // Jumlah default slide yang ditampilkan
+                    slidesPerView={6} // Jumlah default slide yang ditampilkan
                     spaceBetween={30}
                     autoplay={{
                         delay: 3000,
@@ -53,7 +166,7 @@ const Page = (props: Props) => {
                     breakpoints={{
                         // Saat lebar layar >= 1024px
                         1024: {
-                            slidesPerView: 7, // 6 slide
+                            slidesPerView: 6, // 6 slide
                             spaceBetween: 30,
                         },
                         // Saat lebar layar >= 768px
@@ -63,7 +176,7 @@ const Page = (props: Props) => {
                         },
                         // Untuk layar <= 640px, 1 slide akan ditampilkan
                         0: {
-                            slidesPerView: 1, // 1 slide
+                            slidesPerView: 2, // 1 slide
                             spaceBetween: 10,
                         },
                     }}
@@ -74,8 +187,8 @@ const Page = (props: Props) => {
                     {categoryCaraosel.map((item, index) => (
                         <SwiperSlide key={index}>
                             <div className="flex flex-col justify-center items-center">
-                                <div className="w-40 h-40 rounded-full overflow-hidden">
-                                    <Image className="w-full h-full object-cover" src={item.image} alt="shop1" />
+                                <div className="w-30 h-30 rounded-full overflow-hidden">
+                                    <Image onClick={() => handleCategoryClick(item.title)} className="w-full h-full object-cover cursor-pointer" src={item.image} alt="shop1" />
                                 </div>
                                 <p className='text-center mt-2 text-sm' >{item.title}</p>
                             </div>
@@ -93,17 +206,9 @@ const Page = (props: Props) => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-7">
-
-                <CardHover price={1000000} desc='A clean look for a young squad who have their sights set on the very top. Standing out
-                 over those timeless home colours, a simple heat-applied cannon crest is the star of this Arsenal authentic jersey from adidas. 
-                 Stay-cool HEAT.RDY and soft doubleknit fabric combine to keep Gunners players pushing for more on the football pitch. This product
-                  is made with 100% recycled materials. By reusing materials that have already been created, we help to reduce waste and our
-                   reliance on finite resources and reduce the footprint of the products we make.'
-                    title='Arsenal Jersey' image='https://preview.thenewsmarket.com/Previews/ADID/StillAssets/800x600/662664.jpg' />
-
-                {dataShop?.map((item: any, index: number) => (
-                    <CardHover key={index} title={item.name} desc={item.description} image={item?.image[0]} price={item.price} />
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-4 my-7">
+                {filteredData?.map((item: any, index: number) => (
+                    <CardHover location={`/dashboard-super-admin/shop/` + item._id} key={index} title={item.name} desc={item.description} image={item?.image[0]} price={item.price} />
                 ))}
             </div>
 
