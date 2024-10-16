718135db4ef6745b23378c6e3413fbaf57f04220
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-16 08:57:21 +0700

    get building

--- src/app/dashboard-super-admin/building/page.tsx
+++ src/app/dashboard-super-admin/building/page.tsx
@@ -1,7 +1,11 @@
 'use client'
+import { url } from '@/api/auth'
+import { fetcher } from '@/api/fetcher'
 import { bgPengaduan, pembangunan } from '@/app/image'
 import ButtonPrimary from '@/components/elements/buttonPrimary'
+import CardBuilding from '@/components/elements/card/CardBuilding'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
+import { formatDate } from '@/utils/helper'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
@@ -10,11 +14,18 @@ import { IoIosArrowForward } from 'react-icons/io'
 import { MdOutlineCalendarToday } from 'react-icons/md'
 import { Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import useSWR from 'swr'
 
 type Props = {}
 
 const Page = (props: Props) => {
-    const router = useRouter()
+    const { data, error } = useSWR(`${url}/infrastucture/list`, fetcher, {
+        keepPreviousData: true,
+    });
+    const dataPembanguan = data?.data
+    console.log(dataPembanguan);
+
+
     return (
         <DefaultLayout>
             <div className="flex justify-end">
@@ -22,51 +33,12 @@ const Page = (props: Props) => {
 
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-7">
-                <div className="rounded-md hover: border-stroke bg-white  shadow-default  dark:border-strokedark ">
-                    <Swiper
-                        slidesPerView={1} // Jumlah default slide yang ditampilkan
-                        spaceBetween={30}
-                        pagination={{
-                            clickable: true, // Pagination akan bisa diklik
-                        }}
-                        modules={[Pagination]}
-                        className="mySwiper"
-                    >
+                {dataPembanguan?.map((item: any, index: number) => (
+                    <CardBuilding key={index} title={item.name} imageUrl={item.image}
+                        date={formatDate(item.date)} location={item.address}
+                        link={`/dashboard-super-admin/building/${item._id}`} />
+                ))}
 
-                        <SwiperSlide >
-                            <div className="images h-[150px] ">
-                                <Image className='rounded-t-lg w-full h-full' src={pembangunan} alt="jalan rusak" />
-                            </div>
-
-                        </SwiperSlide>
-
-                        <SwiperSlide >
-                            <div className="images h-[150px] ">
-                                <Image className='rounded-t-lg w-full h-full' src={bgPengaduan} alt="jalan rusak" />
-                            </div>
-
-                        </SwiperSlide>
-
-                    </Swiper>
-
-                    <div className="text px-2 py-1 space-y-1">
-                        <h1 className='font-semibold'>Pembuatan aula desa</h1>
-
-                        <div className="flex  items-center gap-1">
-                            <MdOutlineCalendarToday color='#94a3b8' />
-                            <p className='text-sm text-slate-400' >2-4-2024</p>
-                        </div>
-
-                        <div className="flex  items-center gap-1">
-                            <HiMapPin color='#94a3b8' size={15} />
-                            <p className='text-small text-slate-400' >Kp. Tegalkiang No.1 Jawa Barat...</p>
-                        </div>
-
-                    </div>
-                    <div className="flex px-2 justify-end py-2 items-center gap-3 mt-2">
-                        <ButtonPrimary onClick={() => router.push('/dashboard-super-admin/building/12')} className='py-2 px-4 flex justify-center items-center   rounded-xl text-sm lg:text-base'>Selengkap nya <IoIosArrowForward /> </ButtonPrimary>
-                    </div>
-                </div>
             </div>
 
         </DefaultLayout>
---
+++ src/components/elements/card/CardBuilding.tsx
@@ -0,0 +1,70 @@
+'use client'
+import React from 'react'
+import { HiMapPin } from 'react-icons/hi2'
+import { MdOutlineCalendarToday } from 'react-icons/md'
+import { Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import ButtonPrimary from '../buttonPrimary'
+import { useRouter } from 'next/navigation'
+import { IoIosArrowForward } from 'react-icons/io'
+
+type Props = {
+    title: string;
+    imageUrl: string[];
+    date: string;
+    location: string;
+    link: string;
+};
+
+const CardBuilding = ({ title, imageUrl, date, location, link }: Props) => {
+    const router = useRouter()
+
+    return (
+        <div className="rounded-md hover:border-stroke bg-white shadow-default dark:border-strokedark">
+            <Swiper
+                slidesPerView={1} // Jumlah default slide yang ditampilkan
+                spaceBetween={30}
+                pagination={{
+                    clickable: true, // Pagination akan bisa diklik
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+
+                {imageUrl.map((item, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="images h-[150px]">
+                            <img className="rounded-t-lg w-full h-full" src={item} alt="Gambar" />
+                        </div>
+                    </SwiperSlide>
+                ))}
+
+            </Swiper>
+
+            <div className="text px-2 py-1 space-y-1">
+                <h1 className="font-semibold">{title}</h1>
+
+                <div className="flex items-center gap-1">
+                    <MdOutlineCalendarToday color="#94a3b8" />
+                    <p className="text-sm text-slate-400">{date}</p>
+                </div>
+
+                <div className="flex items-center gap-1">
+                    <HiMapPin color="#94a3b8" size={15} />
+                    <p className="text-small text-slate-400">{location}</p>
+                </div>
+            </div>
+
+            <div className="flex px-2 justify-end py-2 items-center gap-3 mt-2">
+                <ButtonPrimary
+                    onClick={() => router.push(link)}
+                    className="py-2 px-4 flex justify-center items-center rounded-xl text-sm lg:text-base"
+                >
+                    Selengkap nya <IoIosArrowForward />
+                </ButtonPrimary>
+            </div>
+        </div>
+    )
+}
+
+export default CardBuilding
