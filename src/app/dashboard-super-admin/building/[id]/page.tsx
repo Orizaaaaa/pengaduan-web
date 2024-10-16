6d0429fb55e8eaf75d088fda6eef1ae842ce9708
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-16 12:51:44 +0700

    create ui update building

--- src/app/dashboard-super-admin/building/[id]/page.tsx
+++ src/app/dashboard-super-admin/building/[id]/page.tsx
@@ -2,17 +2,27 @@
 
 
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import dynamic from 'next/dynamic'
-import { bgPengaduan, pembangunan } from '@/app/image'
+import { bgPengaduan, camera, pembangunan } from '@/app/image'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
 import useSWR from 'swr'
 import { url } from '@/api/auth'
 import { fetcher } from '@/api/fetcher'
 import { useParams, usePathname } from 'next/navigation'
 import { formatDate, formatRupiah, parseCoordinate } from '@/utils/helper'
+import { FaPen, FaTrashAlt } from 'react-icons/fa'
+import InputForm from '@/components/elements/input/InputForm'
+import { useMapEvents } from 'react-leaflet'
+import CaraoselImage from '@/components/fragemnts/caraoselProduct/caraoselProduct'
+import { IoCloseCircleOutline } from 'react-icons/io5'
+import ButtonPrimary from '@/components/elements/buttonPrimary'
+import ButtonSecondary from '@/components/elements/buttonSecondary'
+const MapChoise = dynamic(() => import('@/components/fragemnts/maps/MapChoise'), {
+    ssr: false
+});
 const Map = dynamic(() => import('@/components/fragemnts/maps/Map'), {
     ssr: false
 });
@@ -20,6 +30,7 @@ const Map = dynamic(() => import('@/components/fragemnts/maps/Map'), {
 type Props = {}
 
 const page = (props: Props) => {
+    const [updatePage, setUpdatePage] = useState(false)
     const idBuilding = useParams().id
     const { data, error } = useSWR(`${url}/infrastucture/${idBuilding}`, fetcher, {
         keepPreviousData: true,
@@ -53,75 +64,313 @@ const page = (props: Props) => {
             value: dataBuilding?.address
         },
     ]
+    const [form, setForm] = useState({
+        title: '',
+        description: '',
+        address: '',
+        location: {
+            latitude: 0,
+            longitude: 0,
+        },
+        status: '',
+        image: [] as File[],
+        budget: 0,
+        volume: 0,
+        source_of_funds: '',
+        date: ''
+    });
+
+    useEffect(() => {
+        if (dataBuilding) {
+            setForm({
+                title: dataBuilding.title || '',
+                description: dataBuilding.description || '',
+                address: dataBuilding.address || '',
+                location: {
+                    latitude: dataBuilding.location?.latitude || 0,
+                    longitude: dataBuilding.location?.longitude || 0,
+                },
+                status: dataBuilding.status || '',
+                image: dataBuilding.image,
+                budget: dataBuilding.budget || 0,
+                volume: dataBuilding.volume || 0,
+                source_of_funds: dataBuilding.source_of_funds || '',
+                date: dataBuilding.date || ''
+            });
+        }
+    }, [dataBuilding]);
+
+
+    console.log('ini adalah form', form);
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    // Komponen untuk menangani event klik pada peta
+    const MapEvents = () => {
+        useMapEvents({
+            click(e: any) {
+                // Memperbarui posisi latitude dan longitude di dalam objek location
+                setForm((prevForm) => ({
+                    ...prevForm,
+                    location: {
+                        ...prevForm.location,
+                        latitude: e.latlng.lat,
+                        longitude: e.latlng.lng,
+                    }
+                }));
+            }
+        });
+        return null;
+    };
+
     return (
         <DefaultLayout>
-            <section className=' bg-white p-4 mx-auto py-10 px-5  rounded-md '>
-
-                <h1 className='text-2xl font-bold text-center'> {dataBuilding?.title}</h1>
-
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 my-10">
-                    <div className="rounded-md hover: border-stroke bg-white  shadow-default  dark:border-strokedark">
-                        <Swiper
-                            slidesPerView={1} // Jumlah default slide yang ditampilkan
-                            spaceBetween={30}
-                            pagination={{
-                                clickable: true, // Pagination akan bisa diklik
-                            }}
-                            modules={[Pagination]}
-                            className="mySwiper"
-                        >
-
-                            {dataBuilding?.image.map((item: any, index: number) => (
-                                <SwiperSlide key={index} >
-                                    <div className="images h-[300px]  ">
-                                        <img className='rounded-lg w-full h-full' src={item} alt={item} />
-                                    </div>
-
-                                </SwiperSlide>
-                            ))}
-
-
-
-                        </Swiper>
-                    </div>
-
-                    <div className="text">
-                        <h1 className='text-lg font-medium'>Deskripsi</h1>
-                        <p className='text-sm' > {dataBuilding?.description}</p>
-                    </div>
-
+            <div className=" button-action flex justify-end mb-2 gap-3 ">
+                <div className="flex bg-white justify-end gap-3 p-2 rounded-lg ">
+                    <FaTrashAlt className='cursor-pointer' color='red' />
+                    <FaPen onClick={() => setUpdatePage(prev => !prev)} className='cursor-pointer' color='blue' />
                 </div>
 
-                <section className="detail">
-                    <div className="text space-y-2">
-                        <h1 className='font-medium'>Detail Pembangunan</h1>
+            </div>
+            {updatePage ?
+                <section className=' bg-white p-4 mx-auto py-10 px-5  rounded-md '>
+                    <h1 className='text-2xl font-bold text-center'> {dataBuilding?.title}</h1>
+
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 my-10">
+                        <div className="rounded-md hover: border-stroke bg-white  shadow-default  dark:border-strokedark">
+                            <Swiper
+                                slidesPerView={1} // Jumlah default slide yang ditampilkan
+                                spaceBetween={30}
+                                pagination={{
+                                    clickable: true, // Pagination akan bisa diklik
+                                }}
+                                modules={[Pagination]}
+                                className="mySwiper"
+                            >
+
+                                {dataBuilding?.image.map((item: any, index: number) => (
+                                    <SwiperSlide key={index} >
+                                        <div className="images h-[300px]  ">
+                                            <img className='rounded-lg w-full h-full' src={item} alt={item} />
+                                        </div>
+
+                                    </SwiperSlide>
+                                ))}
+
+
+
+                            </Swiper>
+                        </div>
+
+                        <div className="text">
+                            <h1 className='text-lg font-medium'>Deskripsi</h1>
+                            <p className='text-sm' > {dataBuilding?.description}</p>
+                        </div>
+
+                    </div>
+
+                    <section className="detail">
+                        <div className="text space-y-2">
+                            <h1 className='font-medium'>Detail Pembangunan</h1>
+                            <hr className='w-full text-[#eeeeee]' />
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-5 mt-10">
+                            {dataDetail.map((item, index) => (
+                                <div className="flex  gap-5" key={index}>
+
+                                    <div className="text">
+                                        <h1 className='font-medium' >{item?.title}</h1>
+                                        <h1>{item?.value}</h1>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                    </section>
+
+
+
+                    <div className="text space-y-2 mt-20">
+                        <h1 className='font-medium'>Lokasi Pembangunan</h1>
                         <hr className='w-full text-[#eeeeee]' />
                     </div>
+                    <div className="location mt-5">
+                        <Map lat={parseCoordinate(dataBuilding?.location?.latitude || "0")} lng={parseCoordinate(dataBuilding?.location?.longitude || "0")} />
+                    </div>
+                </section> :
 
-                    <div className="grid grid-cols-2 gap-5 mt-10">
-                        {dataDetail.map((item, index) => (
-                            <div className="flex  gap-5" key={index}>
-
-                                <div className="text">
-                                    <h1 className='font-medium' >{item?.title}</h1>
-                                    <h1>{item?.value}</h1>
-                                </div>
-                            </div>
-                        ))}
+                <section className=' bg-white p-4 mx-auto py-10 px-5  rounded-md '>
+                    <div className="flex justify-center items-center w-full">
+                        <InputForm className='bg-slate-300' type='text' placeholder='Nama Pembangunan' value={form.title} htmlFor='title' onChange={handleChange} />
                     </div>
 
-                </section>
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 my-10">
+                        <div className="rounded-md hover: border-stroke bg-white  shadow-default  dark:border-strokedark">
+                            <div>
+                                <CaraoselImage>
+                                    {form.image.length > 0 ? (
+                                        form.image.map((image: any, index: number) => (
+                                            <SwiperSlide key={index}>
+                                                <>
+                                                    <div className="flex justify-center items-center" style={{ pointerEvents: 'none' }}>
+                                                        <img
+                                                            src={typeof image === 'string' ? image : URL.createObjectURL(image)} // Cek apakah image berupa string atau File
+                                                            alt={`preview-${index}`}
+                                                            className="w-auto h-[200px] relative"
+                                                        />
+                                                    </div>
+                                                    <button
+                                                        className="button-delete array-image absolute top-0 right-0 z-10"
+                                                    >
+                                                        <IoCloseCircleOutline color="red" size={34} />
+                                                    </button>
+                                                </>
+                                            </SwiperSlide>
+                                        ))
+                                    ) : (
+                                        <div className="flex justify-center">
+                                            <Image className="w-auto h-[200px] relative" src={camera} alt="image" />
+                                        </div>
+                                    )}
+                                </CaraoselImage>
+
+                                <div className="grid grid-cols-2 justify-between my-5 gap-2">
+                                    <ButtonPrimary className='rounded-md relative cursor-pointer py-2 px-1' >Tambah Image
+                                        <input
+                                            type="file"
+                                            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                                            id="image-input-add"
+                                        />
+                                    </ButtonPrimary>
+                                    <ButtonSecondary className='rounded-md  py-2 px-1' onClick={() => setForm(prevForm => ({ ...prevForm, name: [] }))} >Hapus Semua</ButtonSecondary>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="text">
+                            <h1 className='text-lg font-medium'>Deskripsi</h1>
+                            <textarea onChange={handleChange} name="description" id="description" cols={30} value={form.description} rows={4}
+                                className="block p-2.5 w-full bg-slate-300 rounded-md outline-none mt-2" ></textarea>
+                        </div>
+
+                    </div>
+
+                    <section className="detail">
+                        <div className="text space-y-2">
+                            <h1 className='font-medium'>Detail Pembangunan</h1>
+                            <hr className='w-full text-[#eeeeee]' />
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-5 mt-10">
+
+                            <div className="grid grid-cols-2 gap-5 mt-10">
+                                <div className="flex  gap-5">
+                                    <div className="text">
+                                        <h1 className='font-medium' >Anggaran</h1>
+                                        <InputForm
+                                            className='bg-slate-300'
+                                            type='text'
+                                            placeholder='Anggaran'
+                                            value={form.budget}
+                                            htmlFor='budget'
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="flex  gap-5">
+                                    <div className="text">
+                                        <h1 className='font-medium' >Sumber Dana</h1>
+                                        <InputForm
+                                            className='bg-slate-300'
+                                            type='text'
+                                            placeholder='Sumber Dana'
+                                            value={form.source_of_funds}
+                                            htmlFor='source_of_funds'
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="flex  gap-5">
+                                    <div className="text">
+                                        <h1 className='font-medium' >Volume</h1>
+                                        <InputForm
+                                            className='bg-slate-300'
+                                            type='text'
+                                            placeholder='Volume'
+                                            value={form.volume}
+                                            htmlFor='volume'
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="flex  gap-5">
+                                    <div className="text">
+                                        <h1 className='font-medium' >Status</h1>
+                                        <InputForm
+                                            className='bg-slate-300'
+                                            type='text'
+                                            placeholder='Status'
+                                            value={form.status}
+                                            htmlFor='status'
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="flex  gap-5">
+                                    <div className="text">
+                                        <h1 className='font-medium' >Tahun</h1>
+                                        <InputForm
+                                            className='bg-slate-300'
+                                            type='text'
+                                            placeholder='Tahun'
+                                            value={form.date}
+                                            htmlFor='date'
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="flex  gap-5">
+                                    <div className="text">
+                                        <h1 className='font-medium' >Alamat</h1>
+                                        <InputForm
+                                            className='bg-slate-300'
+                                            type='text'
+                                            placeholder='Alamat'
+                                            value={form.address}
+                                            htmlFor='address'
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                    </section>
 
 
 
-                <div className="text space-y-2 mt-20">
-                    <h1 className='font-medium'>Lokasi Pembangunan</h1>
-                    <hr className='w-full text-[#eeeeee]' />
-                </div>
-                <div className="location mt-5">
-                    <Map lat={parseCoordinate(dataBuilding?.location?.latitude || "0")} lng={parseCoordinate(dataBuilding?.location?.longitude || "0")} />
-                </div>
-            </section>
+                    <div className="text space-y-2 mt-20">
+                        <h1 className='font-medium'>Lokasi Pembangunan</h1>
+                        <hr className='w-full text-[#eeeeee]' />
+                    </div>
+                    <div className="location mt-5">
+                        <MapChoise markerPosition={{ lat: form.location.latitude, lng: form.location.longitude }} zoom={13} text="Lokasi kejadian" className="h-[370px]  rounded-md mt-4" >
+                            <MapEvents />
+                        </MapChoise>
+                    </div>
+                </section>}
+
         </DefaultLayout>
 
     )
