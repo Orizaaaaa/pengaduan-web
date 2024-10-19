8445e0f6381ea1c80a3a3392b6cb5b78f56a9b85
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-19 23:55:29 +0700

    seacrh

--- src/components/fragemnts/search/Search.tsx
+++ src/components/fragemnts/search/Search.tsx
@@ -11,8 +11,8 @@ type Props = {
 const Search = ({ placeholder, className, onChange }: Props) => {
     return (
         <div className="w-full relative ">
-            <input onChange={onChange} className={` ${className} rounded-xl bg-slate-200 outline-none py-2 ps-11`} type="text" placeholder={placeholder} name="" id="" />
-            <IoSearch size={20} color="#7C7C7C" className="absolute left-3 top-1/2 -translate-y-1/2" />
+            <input onChange={onChange} className={` ${className} rounded-xl bg-white outline-none py-2 ps-11`} type="text" placeholder={placeholder} name="" id="" />
+            <IoSearch size={20} color="black" className="absolute left-3 top-1/2 -translate-y-1/2" />
         </div>
     )
 }
