fc8ccce2fdd9408ed4cc004762798600a1b3b56f
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-22 12:45:50 +0700

    loading reporrt admin

--- src/app/dashboard-super-admin/report/[id]/page.tsx
+++ src/app/dashboard-super-admin/report/[id]/page.tsx
@@ -11,7 +11,7 @@ import MapChoise from '@/components/fragemnts/maps/MapChoise';
 import ModalAlert from '@/components/fragemnts/modal/modalAlert';
 import DefaultLayout from '@/components/layouts/DefaultLayout';
 import { capitalizeWords, formatDate, parseCoordinate } from '@/utils/helper';
-import { AutocompleteItem, Spinner, useDisclosure } from '@nextui-org/react';
+import { AutocompleteItem, Skeleton, Spinner, useDisclosure } from '@nextui-org/react';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 import useSWR, { mutate } from 'swr';
@@ -108,11 +108,6 @@ const Page = () => {
     };
 
 
-
-
-
-
-
     const handleDeleteReport = () => {
         setLoadingDelete(true)
         deleteReport(id, (res: any) => {
@@ -141,98 +136,170 @@ const Page = () => {
 
         })
     }
-
-    console.log(dataReport?.status);
+    const isLoading = !data && !error;
 
     return (
         <DefaultLayout>
             <div className="bg-white p-3">
-                <div className="grid grid-cols-1 lg:grid-cols-2 my-3 gap-8 lg:gap-0">
-                    <div className="left space-y-6">
-                        {dataDetailLaporan.map((item, index) => (
-                            <div className="text" key={index}>
-                                <h1 className="text-lg font-semibold">{item.title}</h1>
-                                <p>{item.text}</p>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="right">
-                        <img
-                            className="h-[170px] md:h-[300px] rounded-md w-auto mx-auto"
-                            src={dataReport?.imageReport[0]}
-                            alt="Report Image"
-                        />
-                    </div>
-                </div>
-
-                <MapChoise
-                    markerPosition={{
-                        lat: parseCoordinate(dataReport?.latitude || "0"),
-                        lng: parseCoordinate(dataReport?.longitude || "0"),
-                    }}
-                    zoom={10}
-                    text={dataReport?.address || ''}
-                    className="h-[370px] rounded-md my-5"
-                />
-
-                <form className="coment" onSubmit={pushComent}>
-                    <h1 className="text-primary font-semibold">Komentar ({dataReport?.comment.length})</h1>
-                    <div className="display-comment bg-slate-300 w-full h-60 rounded-md p-3 overflow-y-auto">
-                        {dataReport?.comment.length === 0 && (
-                            <p className="text-gray-400">Belum ada komentar</p>
-                        )}
-
-                        {dataReport?.comment.map((item: any, index: number) => (
-                            <div key={index}>
-                                {item.name !== name ? (
-                                    <div className="comment my-3 p-2 bg-slate-500 w-fit rounded-tl-2xl rounded-r-xl">
-                                        <p className="text-white text-md">{capitalizeWords(item.name)}</p>
-                                        <p className="text-white text-sm">{item.message}</p>
+                {isLoading ? (
+                    <>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 my-3 gap-8 lg:gap-0">
+                            <div className="left space-y-6">
+                                <div className="max-w-[300px] w-full flex items-center gap-3">
+                                    <div className="w-full flex flex-col gap-2 ">
+                                        <Skeleton className="h-4 w-3/5 rounded-md" />
+                                        <Skeleton className="h-3 w-4/5 rounded-md" />
                                     </div>
-                                ) : (
-                                    <div ref={textRef} className="flex justify-end my-3">
-                                        <div className="comment p-2 bg-primary w-fit rounded-tr-2xl rounded-l-xl">
-                                            <p className="text-white text-md">{capitalizeWords(item.name)}</p>
-                                            <p className="text-white text-sm">{item.message}</p>
-                                        </div>
+                                </div>
+
+                                <div className="max-w-[300px] w-full flex items-center gap-3 mt-4">
+                                    <div className="w-full flex flex-col gap-2 ">
+                                        <Skeleton className="h-4 w-3/5 rounded-md" />
+                                        <Skeleton className="h-3 w-4/5 rounded-md" />
                                     </div>
-                                )}
+                                </div>
+                                <div className="max-w-[300px] w-full flex items-center gap-3 mt-4">
+                                    <div className="w-full flex flex-col gap-2 ">
+                                        <Skeleton className="h-4 w-3/5 rounded-md" />
+                                        <Skeleton className="h-3 w-full rounded-md" />
+                                    </div>
+                                </div>
+                                <div className="max-w-[300px] w-full flex items-center gap-3 mt-4">
+                                    <div className="w-full flex flex-col gap-2 ">
+                                        <Skeleton className="h-4 w-3/5 rounded-md" />
+                                        <Skeleton className="h-3 w-4/5 rounded-md" />
+                                    </div>
+                                </div>
+                                <div className="max-w-[300px] w-full flex items-center gap-3 mt-4">
+                                    <div className="w-full flex flex-col gap-2 ">
+                                        <Skeleton className="h-4 w-3/5 rounded-md" />
+                                        <Skeleton className="h-3 w-4/5 rounded-md" />
+                                    </div>
+                                </div>
+                                <div className="max-w-[300px] w-full flex items-center gap-3 mt-4">
+                                    <div className="w-full flex flex-col gap-2 ">
+                                        <Skeleton className="h-4 w-3/6 rounded-md" />
+                                        <Skeleton className="h-3 w-full rounded-md" />
+                                    </div>
+                                </div>
+                                <div className="max-w-[300px] w-full flex items-center gap-3 mt-4">
+                                    <div className="w-full flex flex-col gap-2 ">
+                                        <Skeleton className="h-4 w-3/5 rounded-md" />
+                                        <Skeleton className="h-3 w-4/5 rounded-md" />
+                                    </div>
+                                </div>
+
                             </div>
-                        ))}
-                    </div>
 
-                    <h1 className="mt-4">Pesan : </h1>
-                    <label htmlFor="message"></label>
-                    <input
-                        className="w-full h-9 rounded-md bg-slate-200 px-3 outline-none"
-                        onChange={handleChangeComent}
-                        type="text"
-                        value={formComent.message}
-                    />
-
-                    <div className="flex justify-end">
-                        <button type="submit" className="bg-primary text-white px-4 py-2 rounded-md mt-4">
-                            Kirim pesan
-                        </button>
-                    </div>
-
-                    <ButtonDelete onClick={openModalDelete} className=' px-4 py-2 rounded-md'>
-                        Hapus Laporan
-                    </ButtonDelete>
-
-                    <div className="flex justify-end items-center gap-3 mt-3">
-                        <div className="flex w-fit justify-center items-center gap-3">
-                            <DropdownCustom defaultSelectedKey={statusFrom} clearButton={false} defaultItems={dataStatus} onSelect={(e: any) => onSelectionChange(e)}>
-                                {(item: any) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
-                            </DropdownCustom>
-
+                            <div className="right">
+                                <Skeleton className="rounded-lg">
+                                    <div className="h-[170px] md:h-[300px] rounded-md w-auto mx-auto bg-default-300"></div>
+                                </Skeleton>
+                            </div>
                         </div>
 
-                        <ButtonPrimary onClick={handleChangeStatus} disabled={loading} className='px-4 py-2 rounded-md flex justify-center items-center'
-                        >{loading ? <Spinner className={`w-5 h-5 mx-8`} size="sm" color="white" /> : 'Ubah Status'}</ButtonPrimary>
+                        <Skeleton className="rounded-lg">
+                            <div className="h-[300px] rounded-md w-full bg-default-300"></div>
+                        </Skeleton>
 
-                    </div>
-                </form>
+                        <Skeleton className="h-4 w-44 rounded-md mt-10 mb-1" />
+                        <Skeleton className="rounded-lg">
+                            <div className="h-[200px] rounded-md w-full bg-default-300"></div>
+                        </Skeleton>
+                    </>
+
+
+                ) : (
+                    <>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 my-3 gap-8 lg:gap-0">
+                            <div className="left space-y-6">
+                                {dataDetailLaporan.map((item, index) => (
+                                    <div className="text" key={index}>
+                                        <h1 className="text-lg font-semibold">{item.title}</h1>
+                                        <p>{item.text}</p>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="right">
+                                <img
+                                    className="h-[170px] md:h-[300px] rounded-md w-auto mx-auto"
+                                    src={dataReport?.imageReport[0]}
+                                    alt="Report Image"
+                                />
+                            </div>
+                        </div>
+
+                        <MapChoise
+                            markerPosition={{
+                                lat: parseCoordinate(dataReport?.latitude || "0"),
+                                lng: parseCoordinate(dataReport?.longitude || "0"),
+                            }}
+                            zoom={10}
+                            text={dataReport?.address || ''}
+                            className="h-[370px] rounded-md my-5"
+                        />
+
+                        <form className="coment" onSubmit={pushComent}>
+                            <h1 className="text-primary font-semibold">Komentar ({dataReport?.comment.length})</h1>
+                            <div className="display-comment bg-slate-300 w-full h-60 rounded-md p-3 overflow-y-auto">
+                                {dataReport?.comment.length === 0 && (
+                                    <p className="text-gray-400">Belum ada komentar</p>
+                                )}
+
+                                {dataReport?.comment.map((item: any, index: number) => (
+                                    <div key={index}>
+                                        {item.name !== name ? (
+                                            <div className="comment my-3 p-2 bg-slate-500 w-fit rounded-tl-2xl rounded-r-xl">
+                                                <p className="text-white text-md">{capitalizeWords(item.name)}</p>
+                                                <p className="text-white text-sm">{item.message}</p>
+                                            </div>
+                                        ) : (
+                                            <div ref={textRef} className="flex justify-end my-3">
+                                                <div className="comment p-2 bg-primary w-fit rounded-tr-2xl rounded-l-xl">
+                                                    <p className="text-white text-md">{capitalizeWords(item.name)}</p>
+                                                    <p className="text-white text-sm">{item.message}</p>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
+
+                            <h1 className="mt-4">Pesan : </h1>
+                            <label htmlFor="message"></label>
+                            <input
+                                className="w-full h-9 rounded-md bg-slate-200 px-3 outline-none"
+                                onChange={handleChangeComent}
+                                type="text"
+                                value={formComent.message}
+                            />
+
+                            <div className="flex justify-end">
+                                <button type="submit" className="bg-primary text-white px-4 py-2 rounded-md mt-4">
+                                    Kirim pesan
+                                </button>
+                            </div>
+
+                            <ButtonDelete onClick={openModalDelete} className=' px-4 py-2 rounded-md'>
+                                Hapus Laporan
+                            </ButtonDelete>
+
+                            <div className="flex justify-end items-center gap-3 mt-3">
+                                <div className="flex w-fit justify-center items-center gap-3">
+                                    <DropdownCustom defaultSelectedKey={statusFrom} clearButton={false} defaultItems={dataStatus} onSelect={(e: any) => onSelectionChange(e)}>
+                                        {(item: any) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
+                                    </DropdownCustom>
+
+                                </div>
+
+                                <ButtonPrimary onClick={handleChangeStatus} disabled={loading} className='px-4 py-2 rounded-md flex justify-center items-center'
+                                >{loading ? <Spinner className={`w-5 h-5 mx-8`} size="sm" color="white" /> : 'Ubah Status'}</ButtonPrimary>
+
+                            </div>
+                        </form>
+                    </>
+                )}
+
             </div>
 
 
