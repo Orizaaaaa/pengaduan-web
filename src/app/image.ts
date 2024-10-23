6a8a0ed73804af374e70d5bacca76f81c904663e
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-23 11:01:35 +0700

    delete image on server

--- src/app/image.ts
+++ src/app/image.ts
@@ -1,41 +1,20 @@
 import logo from '../assets/logo.svg';
 import logo2 from '../assets/logo2.svg';
 import human1 from '../assets/manager2.png';
-import education from '../assets/education.png';
-import garbage from '../assets/garbage.png';
-import hospital from '../assets/hospital.png';
-import medicalKit from '../assets/medical-kit.png';
-import nationalPark from '../assets/national-park.png';
-import noParking from '../assets/no-parking.png';
-import road from '../assets/road.png';
-import tree from '../assets/tree.png';
-import waterPolution from '../assets/water-polution.png';
 import employe from '../assets/employe.png'
 import titleGaleri from '../assets/news1.svg'
-import image1 from '../assets/image1.png'
-import image2 from '../assets/image2.png'
-import image3 from '../assets/image3.png'
 import bgLogin from '../assets/bgLogin.png'
-
 import shop1 from '../assets/shop1.svg'
 import shop2 from '../assets/shop2.svg'
 import shop3 from '../assets/shop3.svg'
 import shop4 from '../assets/shop4.svg'
 import bannerShop from '../assets/bannerShop.svg'
 import shop5 from '../assets/shop5.jpg'
-import pembangunan from '../assets/pembangunan.jpg'
 import shop6 from '../assets/shop6.png'
 import shop7 from '../assets/shop7.png'
 import manusiaLaptop from '../assets/manusiaLaptop.svg'
 import headerShop from '../assets/bgShop.png'
 import headerPengaduan from '../assets/bgPengaduan.png'
-
-import galery1 from '../assets/galery1.jpg'
-import galery2 from '../assets/galery2.jpg'
-import galery3 from '../assets/galery3.jpg'
-import galery4 from '../assets/galery4.png'
-import galery5 from '../assets/galery5.png'
-import galery6 from '../assets/galery6.png'
 import warning from '../assets/warning.png'
 import camera from '../assets/camera.png'
 import basarnas from '../assets/basarnas.png'
@@ -46,7 +25,6 @@ import kejaksaan from '../assets/kejaksaan.png'
 import pertanian from '../assets/pertanian.png'
 import polisi from '../assets/polisi.png'
 import pupr from '../assets/pupr.png'
-import imageLogin from '../assets/imageLogin.png'
 import mkpp from '../assets/mkpp.png'
 import building from '../assets/building.png'
 import galery from '../assets/galery.png'
@@ -55,7 +33,6 @@ import report from '../assets/report.png'
 import onlineShopping from '../assets/online-shopping.png'
 import leftPatern from '../assets/pattern-blue-left.png'
 import rightPatern from '../assets/pattern-blue-white.png'
-
 import bgPengaduan from '../assets/pengaduanBg.png'
 
 
@@ -64,13 +41,10 @@ import bgPengaduan from '../assets/pengaduanBg.png'
 
 
 export {
-    logo, logo2, employe, human1, education, garbage, warning,
-    hospital, medicalKit, nationalPark, noParking, road, tree,
-    waterPolution, titleGaleri, image1, image2, image3, galery1,
-    galery2, galery3, galery4, galery5, galery6,
-    bgPengaduan, imageLogin, camera,
+    logo, logo2, employe, human1, warning, titleGaleri,
+    bgPengaduan, camera,
     headerShop, shop1, shop2, shop3, shop4, shop5, shop6, shop7,
-    headerPengaduan, pembangunan, manusiaLaptop,
+    headerPengaduan, manusiaLaptop,
     basarnas, dinsos, dishub, kejaksaan, pertanian, polisi, pupr, bgLogin, rightPatern,
     bannerShop, allCategori, mkpp, leftPatern, report, building, newspaper, onlineShopping, galery
 };
