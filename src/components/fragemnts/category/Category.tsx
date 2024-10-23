5c1c09f58b39dbebd61eba2ca57c833618ea1ee5
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-23 14:51:03 +0700

    loading category landing

--- src/components/fragemnts/category/Category.tsx
+++ src/components/fragemnts/category/Category.tsx
@@ -3,7 +3,7 @@ import { url } from '@/api/auth'
 import { getCategories } from '@/api/category'
 import { fetcher } from '@/api/fetcher'
 import { categoryData } from '@/utils/dataObject'
-import { Button, Popover, PopoverContent, PopoverTrigger, Tooltip } from '@nextui-org/react'
+import { Button, Popover, PopoverContent, PopoverTrigger, Skeleton, Tooltip } from '@nextui-org/react'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import useSWR from 'swr'
@@ -15,6 +15,8 @@ const Category = () => {
     const dataCategory = data?.data
     console.log(dataCategory);
 
+    const isLoading = !data && !error
+
     return (
 
         <section className='my-15 container mx-auto' id='category'>
@@ -24,28 +26,39 @@ const Category = () => {
             </div>
 
             <div className='grid my-15 grid-cols-3 md:grid-cols-4 lg:grid-cols-8 container mx-auto gap-5'>
-                {dataCategory?.map((item: any, index: number) => (
-                    <Popover placement="top" key={index}>
-                        <PopoverTrigger>
-                            <div className="image flex-col justify-center items-center relative cursor-pointer">
-                                <div className="relative w-25 h-25 md:w-39 md:h-39"> {/* Ukuran tetap untuk membuat gambar bulat */}
-                                    <img
-                                        src={item.image}
-                                        className={`mx-auto rounded-full object-cover w-full h-full shadow-3`} // Hapus padding
-                                        alt='image'
-                                    />
-                                    <div className="absolute inset-0 bg-black opacity-50 rounded-full"></div>
-                                </div>
-                            </div>
-                        </PopoverTrigger>
 
-                        <PopoverContent>
-                            <div className="px-1 py-2">
-                                <div className="text-small">{item.name}</div>
-                            </div>
-                        </PopoverContent>
-                    </Popover>
-                ))}
+                {isLoading ? (
+                    Array.from({ length: 6 }).map((_, index) => (
+                        <div key={index} className='flex flex-col justify-center items-center gap-2'>
+                            <Skeleton className="flex rounded-full w-25 h-25 md:w-39 md:h-39" />
+                            <Skeleton className="h-4 w-full rounded-lg" />
+                        </div>
+                    ))
+                ) : (
+                    dataCategory?.map((item: any, index: number) => (
+                        <Popover placement="top" key={index}>
+                            <PopoverTrigger>
+                                <div className="image flex-col justify-center items-center relative cursor-pointer">
+                                    <div className="relative w-25 h-25 md:w-39 md:h-39"> {/* Ukuran tetap untuk membuat gambar bulat */}
+                                        <img
+                                            src={item.image}
+                                            className={`mx-auto rounded-full object-cover w-full h-full shadow-3`} // Hapus padding
+                                            alt='image'
+                                        />
+                                        <div className="absolute inset-0 bg-black opacity-50 rounded-full"></div>
+                                    </div>
+                                </div>
+                            </PopoverTrigger>
+
+                            <PopoverContent>
+                                <div className="px-1 py-2">
+                                    <div className="text-small">{item.name}</div>
+                                </div>
+                            </PopoverContent>
+                        </Popover>
+                    ))
+                )}
+
             </div>
 
 
