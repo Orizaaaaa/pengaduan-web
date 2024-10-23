5d1f0694071ad4cf3008b3a122eb3e9854490f0e
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-23 15:14:34 +0700

    galery landing loading done

--- src/components/ui/Galery.tsx
+++ src/components/ui/Galery.tsx
@@ -1,12 +1,11 @@
-'use client'
-import React from 'react'
-import Navbar from '../fragemnts/navbar/Navbar'
-import Footer from '../fragemnts/LandingPage/footer/Footer'
-import { Pagination } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import useSWR from 'swr'
-import { fetcher } from '@/api/fetcher'
-import { url } from '@/api/auth'
+'use client';
+import React from 'react';
+import Navbar from '../fragemnts/navbar/Navbar';
+import Footer from '../fragemnts/LandingPage/footer/Footer';
+import useSWR from 'swr';
+import { fetcher } from '@/api/fetcher';
+import { url } from '@/api/auth';
+import { Skeleton } from '@nextui-org/react';
 
 type Props = {}
 
@@ -14,38 +13,47 @@ const Galery = (props: Props) => {
     const { data, error } = useSWR(`${url}/gallery/list`, fetcher, {
         keepPreviousData: true,
     });
-    const dataImage = data?.data
-    console.log(dataImage);
+    const dataImage = data?.data;
+
+    // Menggunakan loading state yang benar
+    const isLoading = !data && !error
 
     return (
         <>
             <Navbar />
-            <section className="image-list  container mx-auto my-20 min-h-[50vh]">
-                <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-4  gap-4">
-                    {dataImage?.map((item: any, index: any) => (
-                        <React.Fragment key={index}>
+            <section className="image-list container mx-auto my-20 min-h-[50vh]">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                    {isLoading ? (
+                        Array.from({ length: 8 }).map((_, index) => (
+                            <Skeleton key={index} className="rounded-lg">
+                                <div className="h-50 w-full rounded-lg bg-default-300"></div>
+                            </Skeleton>
+                        ))
 
-                            {
-                                item.name.map((image: any, index: number) => (
+                    ) : (
+                        dataImage?.map((item: any, index: any) => (
+                            <React.Fragment key={index}>
+                                {item.name.map((image: any, index: number) => (
                                     <div className="cover group relative" key={index}>
                                         <div className="relative h-50">
                                             <img
-                                                src={image} // Mengambil URL langsung dari `image`
+                                                src={image}
                                                 alt={`preview-${image}`}
                                                 className="w-full h-full object-cover rounded-md"
                                             />
                                         </div>
                                     </div>
-                                ))
-                            }
-                        </React.Fragment>
-                    ))}
+                                ))}
+                            </React.Fragment>
+                        ))
+                    )}
+
 
                 </div>
             </section>
             <Footer />
         </>
-    )
+    );
 }
 
-export default Galery
+export default Galery;
