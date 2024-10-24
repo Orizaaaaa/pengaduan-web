b959a1b79ad949def94ee373ec9810b95f62b9ef
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-10-24 17:00:34 +0700

    finish all

--- src/components/ui/LandingUi.tsx
+++ src/components/ui/LandingUi.tsx
@@ -55,7 +55,7 @@ const LandingUi = () => {
                     <section id='beranda'>
                         <div className='container mx-auto min-h-[90vh] lg:min-h-[102vh] grid grid-cols-1 lg:grid-cols-5 items-center px-4 pt-5 md:px-20 overflow-x-hidden'>
                             <div className="title col-span-3 mt-8 lg:mt-0 order-last lg:order-first">
-                                <h1 className='text-3xl lg:text-6xl font-bold text-secondary'>Selamat Datang di website <span className='text-white'>Desa Garut</span></h1>
+                                <h1 className='text-3xl lg:text-6xl font-bold text-secondary'>Selamat Datang di website <span className='text-white'>Kedai Desa</span></h1>
                                 <p className='mt-5 text-sm md:text-base text-white'>
                                     Masyarakat dapat mengadukan permasalahan yang terjadi di garut perihal keamanan, ketertiban umum, kesejahtraan
                                     sosial, pemberdayaan masyarakat, pemerintahan, ekonomi dan pembangunan.
